03f821450d15b354978c06fbcfdc3a2ea53517a5
郭家瑋 <s1101684@gm.pu.edu.tw>
2022-12-25 18:29:45 +0800

    make it better

--- index.py
+++ index.py
@@ -11,19 +11,6 @@ from flask import Flask, render_template, request,make_response, jsonify
 from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
-@app.route("/webhook1764", methods=["POST"])
-def webhook():
-    # build a request object
-    req = request.get_json(force=True)
-    # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
-    return make_response(jsonify({"fulfillmentText": info}))
-
-    if __name__ == "__main__":
-    app.run()
-
 @app.route("/")
 def index():
   homepage = "<h1>郭家瑋的電影搜索網頁</h1>"
@@ -102,6 +89,18 @@ def search():
         return info
     else:  
         return render_template("input.html")
+@app.route("/webhook1764", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+    
+if __name__ == "__main__":
+    app.run()
 
 	elif (action == "CityWeather"):
         city =  req.get("queryResult").get("parameters").get("city")
@@ -116,9 +115,3 @@ def search():
         info += "，溫度：" + MinT + "-" + MaxT + "度"
 
 return make_response(jsonify({"fulfillmentText": info}))
-
-
-
-if __name__ == "__main__":
-    app.run()
-
