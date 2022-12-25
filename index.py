c0a406001561c0f1f3bc558554cd16e59e1708b7
郭家瑋 <s1101684@gm.pu.edu.tw>
2022-12-25 18:55:50 +0800

    make it better

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ db = firestore.client()
 import requests
 from bs4 import BeautifulSoup
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
@@ -89,6 +89,15 @@ def search():
         return info
     else:  
         return render_template("input.html")
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
     app.run()
